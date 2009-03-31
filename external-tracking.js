846581a957fd041c0ed30a0df6071a84cf4fde41
cavemonkey50 <cavemonkey50@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-31 02:34:46 +0000

    Tagging 3.0.2

---
+++ external-tracking.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+
+	jQuery('a').each(function() {
+		var a = jQuery(this);
+		var href = a.attr('href');
+        var url = href.replace('http://','').replace('https://','');
+		var hrefArray = href.split('.').reverse();
+		var extension = hrefArray[0];
+ 
+	 	// If the link is external
+	 	if ( ( href.match(/^http/) ) && ( !href.match(document.domain) ) ) {
+	    	// Add the tracking code
+			a.click(function() {
+				pageTracker._trackPageview(outboundPrefix + url);
+			});
+		}
+	
+	 	// If the link is a download
+		if (jQuery.inArray(extension,fileTypes) != -1) {
+			// Add the tracking code
+			a.click(function() {
+				pageTracker._trackPageview(downloadsPrefix + url);
+			});
+		}
+	});
+
+});
